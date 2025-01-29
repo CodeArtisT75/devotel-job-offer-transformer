d08856aa70a666ca1ac358edae82d931201c03a6
CodeArtisT75 <mh.javanbakht.75@gmail.com>
2025-01-29 17:43:57 +0330

    chore: add config module, sequelize, swagger and helmet

---
+++ src/lib/utils/swagger.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function registerSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Devotel Job Offer Transformer API')
+    .setDescription('API documentation for Devotel Job Offer Transformer')
+    .setVersion('1.0')
+    .addTag('Home')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { registerSwagger } from './lib/utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  registerSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
